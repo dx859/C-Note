158e98c80d36a0cee4403c565fc14130c4a45c61
dx859 <dx859@gmail.com>
2016-03-27 22:49:12 +0800

    add linux

---
+++ C++/base/lesson4/a.h
@@ -0,0 +1,6 @@
+#ifndef AH_H
+#define AH_H
+
+void test();
+
+#endif
---
+++ C++/base/lesson4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "a.h"
+
+using namespace std;
+
+int main(void) {
+    test();
+    cout << "hello world!" << endl;
+    return 0;
+}
