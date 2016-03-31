f03ad08bf6f04c66152aeb7b2bfcb492cbb35301
dx859 <daixi859@gmail.com>
2016-03-31 21:34:59 +0800

    left and right for c++

---
+++ C++/base/lesson4/01main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(void) {
+
+    // -1 ? system("calc") : system("mspaint");
+
+    int a = 3;
+
+    // 在C语言中不合法
+    
+    // (a = 3) = 4; 
+
+    std::cout << a << std::endl;
+
+    return 0;
+}
